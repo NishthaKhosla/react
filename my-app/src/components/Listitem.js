43c3a1eb0d33fc473fd41a0d20278b4e4efe5693
Nishtha Khosla <nishthakhosla77@gmail.com>
2024-01-18 23:44:00 +0530

    changes

--- my-app/src/components/Listitem.js
+++ my-app/src/components/Listitem.js
@@ -7,8 +7,8 @@ const ListItem = ({ data }) => {
   const [message, setMessage] = useState("Add to cart");
   var [count, setCount] = useState(0);
   const [modal, setModal] = useState(false);
-  const decreaseCount = () => {
 
+  const decreaseCount = () => {
     setCount((prev) => {
       return prev <= 1 ? 0 : prev - 1;
     });
@@ -24,12 +24,12 @@ const ListItem = ({ data }) => {
     setCount((prev) => prev + 1);
   };
 
-  const handleModal =( ) =>{
-    setModal(prev => !prev);
-  }
+  const handleModal = () => {
+    setModal((prev) => !prev);
+  };
   return (
     <Fragment>
-      <div onClick = {handleModal} className={"item-card"}>
+      <div onClick={handleModal} className={"item-card"}>
         <img
           className={"img-fluid"}
           src={`./assets/${data.thumbnail}`}
@@ -48,10 +48,6 @@ const ListItem = ({ data }) => {
           <div className={"Description"}>
             <span>{data.title}</span>
           </div>
-          {/* <button className="sizes">{data.size1}</button>
-        <button className="sizes">{data.size2}</button>
-        <button className="sizes">{data.size3}</button>
-        <button className="sizes">{data.size4}</button> */}
         </div>
 
         {count < 1 ? (
@@ -74,50 +70,58 @@ const ListItem = ({ data }) => {
         )}
       </div>
 
-      {modal && 
-      <Modal onClose={handleModal}> 
-      <div className="item-card__modal">
-        <div className="img-wrap">
-        <img
-          className={"img-fluid"}
-          src={`./assets/${data.thumbnail}`}
-          alt="no-img"
-        />
-        <div className="meta">
-          <h3>{data.title }</h3>
-          <div className={"pricing"}>
-            <span>${data.discountedPrice}</span>
-            <small>
-              <strike>${data.price}</strike>
-            </small>
+      {modal && (
+        <Modal onClose={handleModal}>
+          <div className="item-card__modal">
+            <div className="img-wrap">
+              <img
+                className={"img-fluid"}
+                src={`./assets/${data.thumbnail}`}
+                alt="no-imgs"
+              />
+              <div className="meta">
+                <h3>{data.title}</h3>
+                <div className={"pricing"}>
+                  <span>${data.discountedPrice}</span>
+                  <small>
+                    <strike>${data.price}</strike>
+                  </small>
+                </div>
+                <div className={"title"}>
+                  <h3>{data.Title}</h3>
+                </div>
+                <div className={"Description"}>
+                  <span>{data.description}</span>
+                </div>
+              </div>
+              {count < 1 ? (
+                <button className={"cart-add"} onClick={increaseCount}>
+                  <h3 className={"cart-text"}>{message}</h3>
+                  <img className={"cart-img"} src={AddToCart} alt="cart" />
+                </button>
+              ) : (
+                <div className={"counter"}>
+                  <span className={"count-text"}>
+                    <button
+                      className={"count-btn btn1"}
+                      onClick={decreaseCount}
+                    >
+                      -
+                    </button>
+                    <span className={"count-value"}>{count}</span>
+                    <button
+                      className={"count-btn btn2"}
+                      onClick={increaseCount}
+                    >
+                      +
+                    </button>
+                  </span>
+                </div>
+              )}
+            </div>
           </div>
-          <div className={"title"}>
-            <h3>{data.Title}</h3>
-          </div>
-          <div className={"Description"}>
-            <span>{data.description}</span>
-          </div>
-        </div>
-        {count < 1 ? (
-          <button className={"cart-add"} onClick={increaseCount}>
-            <h3 className={"cart-text"}>{message}</h3>
-            <img className={"cart-img"} src={AddToCart} alt="cart" />
-          </button>
-        ) : (
-          <div className={"counter"}>
-            <span className={"count-text"}>
-              <button className={"count-btn btn1"} onClick={decreaseCount}>
-                -
-              </button>
-              <span className={"count-value"}>{count}</span>
-              <button className={"count-btn btn2"} onClick={increaseCount}>
-                +
-              </button>
-            </span>
-          </div>
-        )}
-
-          </div></div> </Modal>}
+        </Modal>
+      )}
     </Fragment>
   );
 };
--- my-app/src/components/UI/modal.js
+++ my-app/src/components/UI/modal.js
@@ -1,25 +1,28 @@
 //fragments are used to return multiple elements makes the syntax shorter if we dont dont want to make multiple nodes
-import {Fragment } from "react";
+
+import { Fragment } from "react";
 import ReactDom from "react-dom";
-import Backdrop from "./loader"
-import "./modal.css"
-const Modal =({onClose,children})=>{
-    return(
+import Backdrop from "./loader";
+import "./modal.css";
+
+const Modal = ({ onClose, children }) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(
         <Fragment>
-{
-    ReactDom.createPortal(
-        <Fragment>
-            <Backdrop/>
-            <div className="modal">{children}
-            <button  type= "close" onClick = {onClose}>X</button></div>
-            <div className="content"> {children}</div>
-            
+          <Backdrop />
+          <div className="modal">
+            {children}
+            <button type="close" onClick={onClose}>
+              X
+            </button>
+          </div>
+          <div className="content"> {children}</div>
         </Fragment>,
         document.getElementById("modal-root")
-    )
-}
-        </Fragment>
-    )
-}
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
